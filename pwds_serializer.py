36829e1e0655b07b5ce3b21b4c0f6a2c25ef8593
Agata Szydełko <agata.szydelko@outlook.com>
2024-06-07 13:32:59 +0200

    progress

--- pwds_serializer.py
+++ pwds_serializer.py
@@ -2,24 +2,35 @@ import serial
 import time
 
 
-class Serializer:
+class PWDS_Serializer:
     def __init__(self):
         self.port = '\\\\.\\COM6'
         self.baudrate = 9600
         self.bytesize = serial.EIGHTBITS
         self.stopbits = serial.STOPBITS_ONE
 
+        self.send_frame()
+
+    def send_frame(self):
         ser = serial.Serial(self.port, self.baudrate, bytesize=self.bytesize, stopbits=self.stopbits, timeout=1)
         ser.setRTS = True
 
         command = b'\x10\x02\x00\x00\x00\x00\x10\x03'
-        ser.write(command)
-        time.sleep(2)
 
-        response = ser.read(100)
+        while True:
+            ser.write(command)
+            response = ser.read(100)
 
-        print(response)
+            ascii_start = 8
+            ascii_end = len(response)-2
+            ascii_bytes = response[ascii_start:ascii_end]
+            ascii_representation = ascii_bytes.decode('ascii')
+            print("Received:", ascii_representation)
 
+            time.sleep(1)
+
+    def stop_program(self):
+        ser = serial.Serial(self.port, self.baudrate, bytesize=self.bytesize, stopbits=self.stopbits, timeout=1)
         ser.close()
 
 
